92a7da8427456b4ec03491308bbbebf4f98e15d7
sanju-we <pnsanju8@gmail.com>
2025-12-13 10:57:11 +0530

    types updat reverte

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,8 @@
-declare module 'express';
-declare module 'cors';
-declare module 'cookie-parser';
-declare module 'morgan';
-declare module 'bcrypt';
-declare module 'streamifier';
-declare module 'multer';
-declare var process: any;
+// declare module 'express';
+// declare module 'cors';
+// declare module 'cookie-parser';
+// declare module 'morgan';
+// declare module 'bcrypt';
+// declare module 'streamifier';
+// declare module 'multer';
+// declare var process: any;
